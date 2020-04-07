97407d992f6ef0514433da4fe9b8bac2c4b97672
Jonathan Lowe <jonathanglowe@gmail.com>
2020-04-07 16:46:37 -0400

    Added initial config for minecraft

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "digitalocean" {
+  api_endpoint = "https://api.digitalocean.com"
+}
+
+resource "digitalocean_droplet" "minecraft1" {
+  image    = "centos-8-x64"
+  name     = "minecraft1"
+  region   = "nyc1"
+  size     = "s-1vcpu-2gb"
+  ssh_keys = [24386082, 16891978, 23631845]
+}
+
+resource "digitalocean_domain" "domain" {
+  name = "scrabblicious.com"
+}
+
+resource "digitalocean_record" "minecraft1" {
+  domain = digitalocean_domain.domain.name
+  type   = "A"
+  ttl    = "60"
+  name   = "minecraft1"
+  value  = digitalocean_droplet.minecraft1.ipv4_address
+}
+
+resource "digitalocean_firewall" "minecraft_firewall" {
+  name = "only-22-and-25565"
+
+  droplet_ids = [digitalocean_droplet.minecraft1.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "25565"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
