1fe0703bd99d743898ae1b1708428bcdb83955db
Jonathan Lowe <me@jonathanlowe.dev>
2021-06-19 23:13:06 -0400

    Replaced the salt install with pure ansible

---
+++ ansible/library/minecraft_server_data.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+import requests
+import hashlib
+from ansible.module_utils.basic import *
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: my_test
+
+short_description: This is my test module
+
+version_added: "2.4"
+
+description:
+    - "This is my longer description explaining my test module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the test module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - Your Name (@yourhandle)
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  my_test:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_test:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_test:
+    name: fail me
+'''
+
+RETURN = '''
+original_message:
+    description: The original name param that was passed in
+    type: str
+    returned: always
+message:
+    description: The output message that the test module generates
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        version=dict(type='str', required=True),
+    )
+
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        url='',
+        hash_digest=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    http_result = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+
+    version_data = next(version_data["url"] for version_data in http_result.json()["versions"] if version_data["id"] == module.params['version'])
+    url = requests.get(version_data).json()["downloads"]["server"]["url"]
+
+    hash_digest = hashlib.md5(requests.get(url).content).hexdigest()
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['url'] = url
+    result['hash_digest'] = hash_digest
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
+
